772d1d591b0e816997b34b6f4ebab6bffd6e0fed
TarunS-2003 <95118533+TarunS-2003@users.noreply.github.com>
2023-05-28 03:56:48 +0000

    Added dashboard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,url_for,redirect
+from flask import Flask,render_template,url_for,redirect,request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin,LoginManager,login_user,current_user,login_required,logout_user
 from flask_wtf import FlaskForm
@@ -28,6 +28,15 @@ class User(db.Model,UserMixin):
   id = db.Column(db.Integer,primary_key = True)
   username = db.Column(db.String(25),nullable = False,unique = True)
   password = db.Column(db.String(80),nullable = False,unique = True)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Item('{self.name}', {self.quantity}, {self.price})"
 
 class SignupForm(FlaskForm):
   username = StringField(validators = [InputRequired(), Length(min = 4,max = 20)], render_kw={"placeholder":"Username"})
@@ -101,12 +110,39 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route("/dashboard", methods = ['GET','POST'])
+@app.route('/dashboard')
+
+def dashboard():
+    items = Item.query.all()
+    return render_template('dashboard.html', items=items)
+
+@app.route('/add', methods = ['GET','POST'])
 
 @login_required
 
-def dashboard():
-  return render_template("dashboard.html")
+def add():
+    if request.method == 'POST':
+        name = request.form['name']
+        quantity = int(request.form['quantity'])
+        price = float(request.form['price'])
+
+        item = Item(name=name, quantity=quantity, price=price)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('dashboard'))
+    
+    return render_template('add.html')
+
+
+@app.route('/drop/<int:item_id>', methods=['POST'])
+
+@login_required
+
+def drop(item_id):
+    item = Item.query.get(item_id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('dashboard'))
 
 #version1
 if __name__ == '__main__':
